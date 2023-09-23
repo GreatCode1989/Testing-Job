c035619b24a6091b0c7eb3b679fdb4625b5442ca
GreatCode1989 <wef666wef6@gmail.com>
2023-09-23 08:57:29 +0300

    lo

--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,22 @@
 
-
 document.addEventListener("DOMContentLoaded", function () {
-    var gridItems = document.querySelectorAll(".grid-item");
-    var popup = document.getElementById("popup");
+  var gridItems = document.querySelectorAll(".grid-item");
+  var popup = document.getElementById("popup");
+  var exitButton = document.querySelector(".exit"); 
   
-    gridItems.forEach(function (item) {
-      item.addEventListener("click", function () {
-        popup.style.display = "block"; // Открываем всплывающее окно
-      });
-    });
-  
-    popup.addEventListener("click", function () {
-      popup.style.display = "none"; // Закрываем всплывающее окно при клике на него
+  gridItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      popup.style.display = "block"; 
     });
   });
-  
+
+  exitButton.addEventListener("click", function () {
+    popup.style.display = "none"; 
+  });
+
+  popup.addEventListener("click", function (event) {
+    if (event.target === popup) {
+      popup.style.display = "none"; 
+    }
+  });
+});
